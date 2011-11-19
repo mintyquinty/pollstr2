e08c9acc3767f79df6bf313b97832b9c5e88ab85
Quintan Park <qpark@current.com>
2011-11-18 18:12:43 -0800

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CurrentPollstr::Application.config.secret_token = 'f613938502093b7189203c98a0df08070b90e99003e57852aa0b6cf3e80fa551f64a2c615f071cf77db3c63b80dffce1153a6cb423a7200314e2a9e67e889acb'
